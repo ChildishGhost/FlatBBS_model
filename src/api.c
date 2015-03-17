134999680387cc1f33c2f2b85bf3712385f508ad
xatier <xatierlike@gmail.com>
2015-03-18 01:30:01 +0800

    move JSON related stuffs into json.h and json.c

--- src/api.c
+++ src/api.c
@@ -1,4 +1,5 @@
 #include "api.h"
+#include "json.h"
 
 #include <stdlib.h>
 #include <string.h>
@@ -7,93 +8,66 @@
 char *board_list (const unsigned offset, const unsigned length) {
     fprintf(stdout, "%s\n", __func__);
 
-    char *buf = malloc(10);
-    strcpy(buf, "stub");
-
-    return buf;
+    return make_stub();
 }
 
 char *board_length (void) {
     fprintf(stdout, "%s\n", __func__);
 
     unsigned board_length = 5566;
-    char *buf = malloc(50);
-    sprintf(buf, "{ \"length\" : %d }", board_length);
-    return buf;
+
+    return make_json(50, "{ \"length\" : %d }", board_length);
 }
 
 // all posts of one board
 char *board_post_list(const unsigned bid, const unsigned offset, const unsigned length) {
     fprintf(stdout, "%s\n", __func__);
 
-    char *buf = malloc(10);
-    strcpy(buf, "stub");
-
-    return buf;
+    return make_stub();
 }
 
 char *board_post_length(const unsigned bid) {
     fprintf(stdout, "%s\n", __func__);
 
-    char *buf = malloc(10);
-    strcpy(buf, "stub");
-
-    return buf;
+    return make_stub();
 }
 
 // all inner posts of a board
 char *board_inner_post_list (const unsigned bid, const unsigned offset, const unsigned length) {
     fprintf(stdout, "%s\n", __func__);
 
-    char *buf = malloc(10);
-    strcpy(buf, "stub");
-
-    return buf;
+    return make_stub();
 }
 
 char *board_inner_post_length (const unsigned bid) {
     fprintf(stdout, "%s\n", __func__);
 
-    char *buf = malloc(10);
-    strcpy(buf, "stub");
-
-    return buf;
+    return make_stub();
 }
 
 // favorite boards list of a user
 char *user_fav_list (const unsigned uid, const unsigned offset, const unsigned length) {
     fprintf(stdout, "%s\n", __func__);
 
-    char *buf = malloc(10);
-    strcpy(buf, "stub");
-
-    return buf;
+    return make_stub();
 }
 
 char *user_fav_list_length (const unsigned uid) {
     fprintf(stdout, "%s\n", __func__);
-    char *buf = malloc(10);
-    strcpy(buf, "stub");
 
-    return buf;
+    return make_stub();
 }
 
 // load or save a post, save if aid_index = -1
 char *board_post_path (const unsigned bid, const long aid_index) {
     fprintf(stdout, "%s\n", __func__);
 
-    char *buf = malloc(10);
-    strcpy(buf, "stub");
-
-    return buf;
+    return make_stub();
 }
 
 // the list of nodes belong to given class path
 char *class_items_list (const char *cpath) {
     fprintf(stdout, "%s\n", __func__);
 
-    char *buf = malloc(10);
-    strcpy(buf, "stub");
-
-    return buf;
+    return make_stub();
 }
---
+++ src/json.c
@@ -0,0 +1,66 @@
+#include "json.h"
+
+#include <stdarg.h>
+
+// shortcuts for jansson APIs
+static json_t     *(*_get)(const json_t *, const char *) = json_object_get;
+static const char *(*_s)(const json_t *) = json_string_value;
+static size_t      (*_s_len)(const json_t *) = json_string_length;
+static json_int_t  (*_i)(const json_t *) = json_integer_value;
+static double      (*_r)(const json_t *) = json_real_value;
+
+static json_t *json_ctor (const char *text) {
+
+    // TODO: add needed flags and error handling code here
+    json_error_t error;
+    json_t *root = json_loads(text, 0, &error);
+
+    return json_is_null(root) ? NULL : root;
+}
+
+// alias of ref--
+static void json_dtor (json_t *j) {
+    json_decref(j);
+}
+
+// get API name from JSON: { "api" : "name" , ...}
+char *get_API_name (const char *buf_i) {
+
+    json_t *root = json_ctor(buf_i);
+
+    const char *str  = _s(_get(root, "api"));
+    size_t sz = _s_len(_get(root, "api"));
+
+    char *name = (char *)malloc(sz + 1);
+
+    if (name) {
+        memcpy(name, str, sz);
+        name[sz] = '\0';
+    }
+
+    json_dtor(root);
+    return name;
+}
+
+// empty json stub for testing
+char *make_stub (void) {
+
+    char *buf = malloc(10);
+    if (buf)
+        strcpy(buf, "{ }");
+
+    return buf;
+}
+
+// allocate a fixed size json string with parameters
+char *make_json (unsigned sz, const char *fmt, ...) {
+
+    char *buf = malloc(sz);
+    if (buf) {
+        va_list ap;
+        va_start(ap, fmt);
+        vsprintf(buf, fmt, ap);
+        va_end(ap);
+    }
+    return buf;
+}
