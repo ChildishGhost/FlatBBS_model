65acaec5b134a16b03af6fa32e163abd2032b485
xatier <xatierlike@gmail.com>
2015-03-19 16:43:09 +0800

    JSON wrappers for some board APIs

--- include/json.h
+++ include/json.h
@@ -1,4 +1,5 @@
 #include "struct.h"
+#include "user.h"
 
 #include <jansson.h>
 
@@ -10,11 +11,22 @@
 
 // json utilities, wrappers for the jansson library
 //
-// all interface should be C types like int or char *
+// pack/unpack JSON between data
+//
+// all interface should be C types like int or char * or utf8 *
 
+// board
+char *get_name (const char *);
+utf8 *get_category (const char *);
+utf8 *get_title (const char *);
+unsigned *get_masters (const char *);
+unsigned get_masters_count (const char *);
+
+// test
 char *get_param (const char *);
-char *get_API_name (const char *);
 
+// utilities
+char *get_API_name (const char *);
 char *make_stub (void);
 char *make_json (unsigned, const char *, ...);
 
--- src/json.c
+++ src/json.c
@@ -38,10 +38,22 @@ static char *_json_get_string (const char *buf_i, const char *key) {
     return value;
 }
 
+static utf8 *_json_get_string_u (const char *buf_i, const char *key, size_t sz, unsigned len) {
+    char *c_str =  _json_get_string(buf_i, key);
+    utf8 *u_str = (utf8 *)malloc(sz);
+
+    if (c_str && u_str) {
+        u_uastrncpy(u_str, c_str, len);
+    }
+    free(c_str);
+
+    return u_str;
+}
+
 static long long _json_get_integer (const char *buf_i, const char *key) {
     json_t *root = json_ctor(buf_i);
 
-    int value = _i(_get(root, key));
+    long long value = _i(_get(root, key));
 
     json_dtor(root);
     return value;
@@ -56,6 +68,56 @@ static long long _json_get_floating (const char *buf_i, const char *key) {
     return value;
 }
 
+
+char *get_name (const char *buf_i) {
+    return _json_get_string(buf_i, "name");
+}
+
+utf8 *get_category (const char *buf_i) {
+    return _json_get_string_u(buf_i, "category",
+                              member_size(struct BRD, category),
+                              member_length(struct BRD, category, utf8));
+}
+
+utf8 *get_title (const char *buf_i) {
+    return _json_get_string_u(buf_i, "title",
+                              member_size(struct BRD, title),
+                              member_length(struct BRD, title, utf8));
+}
+
+unsigned int *get_masters (const char *buf_i) {
+    json_t *root = json_ctor(buf_i);
+
+    json_t *masters_arr = _get(root, "masters");
+
+    unsigned count = get_masters_count(buf_i);
+    unsigned *masters = (unsigned *)malloc(member_size(struct BRD, masters));
+
+    for (int i = 0; i < member_length(struct BRD, masters, unsigned); i++) {
+        masters[i] = count ? user_name2id(_s(json_array_get(masters_arr, i))) :
+                             0;
+    }
+
+    json_dtor(root);
+    return masters;
+}
+
+unsigned get_masters_count (const char *buf_i) {
+    json_t *root = json_ctor(buf_i);
+
+    json_t *masters_arr = _get(root, "masters");
+    unsigned int count = 0;
+
+    if (json_is_array(masters_arr)) {
+        count = json_array_size(masters_arr);
+        count = count <= 16 ? count : 16;
+    }
+
+    json_dtor(root);
+    return count;
+}
+
+
 char *get_param (const char *buf_i) {
     return _json_get_string(buf_i, "param");
 }
@@ -68,7 +130,7 @@ char *get_API_name (const char *buf_i) {
 // empty json stub for testing
 char *make_stub (void) {
 
-    char *buf = malloc(10);
+    char *buf = (char *)malloc(10);
     if (buf)
         strcpy(buf, "{ }");
 
@@ -78,7 +140,7 @@ char *make_stub (void) {
 // allocate a fixed size json string with parameters
 char *make_json (unsigned sz, const char *fmt, ...) {
 
-    char *buf = malloc(sz);
+    char *buf = (char *)malloc(sz);
     if (buf) {
         va_list ap;
         va_start(ap, fmt);
