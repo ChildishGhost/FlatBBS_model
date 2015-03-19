5b2d97d6f84673316e97cb7f45a3d5b20290fa64
xatier <xatierlike@gmail.com>
2015-03-19 16:43:53 +0800

    stub for user_name2id

---
+++ include/user.h
@@ -0,0 +1,8 @@
+#include "struct.h"
+
+#ifndef USER_H
+#define USER_H
+
+unsigned user_name2id (const char *);
+
+#endif /* USER_H */
---
+++ src/user.c
@@ -0,0 +1,11 @@
+#include "user.h"
+
+unsigned user_name2id (const char *username) {
+    if (!username) {
+        return 0;
+    }
+
+    // TODO: return USER_BASE.uid
+    return 0;
+
+}
