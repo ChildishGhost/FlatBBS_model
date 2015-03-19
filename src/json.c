6fbb4df39cf5baf7b1497f217cf33242e45d3235
xatier <xatierlike@gmail.com>
2015-03-19 14:40:30 +0800

    refactor `json.c`

--- src/json.c
+++ src/json.c
@@ -5,7 +5,7 @@ static json_t     *(*_get)(const json_t *, const char *) = json_object_get;
 static const char *(*_s)(const json_t *) = json_string_value;
 static size_t      (*_s_len)(const json_t *) = json_string_length;
 static json_int_t  (*_i)(const json_t *) = json_integer_value;
-static double      (*_r)(const json_t *) = json_real_value;
+static double      (*_f)(const json_t *) = json_real_value;
 
 static json_t *json_ctor (const char *text) {
 
@@ -21,23 +21,44 @@ static void json_dtor (json_t *j) {
     json_decref(j);
 }
 
-// get API name from JSON: { "api" : "name" , ...}
-char *get_API_name (const char *buf_i) {
-
+static char *_json_get_string (const char *buf_i, const char *key) {
     json_t *root = json_ctor(buf_i);
 
-    const char *str  = _s(_get(root, "api"));
-    size_t sz = _s_len(_get(root, "api"));
+    const char *str  = _s(_get(root, key));
+    size_t sz = _s_len(_get(root, key));
 
-    char *name = (char *)malloc(sz + 1);
+    char *value = (char *)malloc(sz + 1);
 
-    if (name) {
-        memcpy(name, str, sz);
-        name[sz] = '\0';
+    if (value) {
+        memcpy(value, str, sz);
+        value[sz] = '\0';
     }
 
     json_dtor(root);
-    return name;
+    return value;
+}
+
+static long long _json_get_integer (const char *buf_i, const char *key) {
+    json_t *root = json_ctor(buf_i);
+
+    int value = _i(_get(root, key));
+
+    json_dtor(root);
+    return value;
+}
+
+static long long _json_get_floating (const char *buf_i, const char *key) {
+    json_t *root = json_ctor(buf_i);
+
+    int value = _f(_get(root, key));
+
+    json_dtor(root);
+    return value;
+}
+
+// get API name from JSON: { "api" : "name" , ...}
+char *get_API_name (const char *buf_i) {
+    return _json_get_string(buf_i, "api");
 }
 
 // empty json stub for testing
