1d0bef5d37e66f95b1c0a904feada4f61c7da322
xatier <xatierlike@gmail.com>
2015-04-03 03:42:17 +0800

    define the constant for data path

--- include/config.h
+++ include/config.h
@@ -1,12 +1,14 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#define DATA_PATH "/home/xatier/work/flatbbs/model/data/"
+
 // brd is the directory for boards
 // BRD is an array of board headers
-#define brd_PATH "/home/xatier/work/flatbbs/model/data/brd/"
-#define BRD_PATH "/home/xatier/work/flatbbs/model/data/BRD"
+#define brd_PATH DATA_PATH "brd/"
+#define BRD_PATH DATA_PATH "BRD"
 
 // USR is an array of user headers
-#define USR_PATH "/home/xatier/work/flatbbs/model/data/USR"
+#define USR_PATH DATA_PATH "USR"
 
 #endif /* CONFIG_H */
