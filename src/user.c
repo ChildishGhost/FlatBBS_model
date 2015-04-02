05e974ffc9675f1d374e9a1e07c26148dfca5519
sifmelcara <rbdn@9mm.davy.tw>
2015-04-02 23:07:02 +0800

    add create_usr function

--- src/user.c
+++ src/user.c
@@ -24,3 +24,42 @@ char *user_uid2name (const unsigned uid) {
 
     return username;
 }
+
+// return value: uid
+int create_usr(const utf8 *username, const char *password, const utf8 *usernick,
+               const char *email, const unsigned mounth, const unsigned day, const enum SEX sex) {
+    struct USER_BASE *new_usr = (struct USER_BASE *)malloc(sizeof(struct USER_BASE)) ;
+    int idx = -1 ;
+    if (new_usr) {
+        memcpy(new_usr->username, username, member_size(struct USER_BASE, username)) ;
+        memcpy(new_usr->password, password, member_size(struct USER_BASE, password)) ;
+        memcpy(new_usr->usernick, usernick, member_size(struct USER_BASE, usernick)) ;
+        memcpy(new_usr->email, email, member_size(struct USER_BASE, email)) ;
+        new_usr->extra.mounth = mounth ;
+        new_usr->extra.day = day ;
+        new_usr->extra.sex = sex ;
+         
+        new_usr->from = NULL ;
+        new_usr->perm = PERM_USER ;
+        // ufo initialize ??
+        new_usr->signature = 0 ; // default signature file number
+        new_usr->num_logins = 0 ;
+        new_usr->num_posts = 0 ;
+        new_usr->good_article = 0 ;
+        new_usr->bad_article = 0 ;
+        new_usr->first_login = time(NULL) ;
+        new_usr->last_login = time(NULL) ;
+        new_usr->staytime = 0ULL ;
+        // log initialize ??
+        new_usr->extra.money = 0 ;
+        new_usr->extra.gold = 0 ;
+
+        idx = USR_length() ;
+        idx = save_usr(new_usr, idx) ;
+
+        free(new_usr) ;
+    }
+
+    return idx ;
+}
+
