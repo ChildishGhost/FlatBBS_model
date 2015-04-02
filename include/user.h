611263a1da4a764574adabcc1e090784ffcde082
xatier <xatierlike@gmail.com>
2015-04-03 03:45:33 +0800

    style fix

--- include/user.h
+++ include/user.h
@@ -6,16 +6,16 @@
 unsigned user_name2uid (const char *);
 char *user_uid2name (const unsigned);
 
-int create_usr(const char *, const char *, const utf8 *,
-               const char *, const unsigned, const unsigned, const enum SEX) ;
+int create_usr (const char *, const char *, const utf8 *,
+               const char *, const unsigned, const unsigned, const enum SEX);
 
-int save_usr(struct USER_BASE *, int) ;
+int save_usr (struct USER_BASE *, int);
 
-struct USER_BASE *load_usr(int) ;
+struct USER_BASE *load_usr(int);
 
-int USR_length(void) ;
+int USR_length (void);
 
-char *usr_perm2text(struct USER_BASE *) ;
-char *usr_sex2text (struct USER_BASE *) ;
+char *usr_perm2text (struct USER_BASE *);
+char *usr_sex2text (struct USER_BASE *);
 
 #endif /* USER_H */
